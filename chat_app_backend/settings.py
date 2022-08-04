ecb2d3c27cbc688cddc8044ae76f6be95306981a
Stefan <sper1234@gmail.com>
2022-08-04 17:28:15 +0200

    Refactoring

--- chat_app_backend/settings.py
+++ chat_app_backend/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 # MEDIA_ROOT = 'C:/Users/Stefan/Desktop/python/chat_app_backend/avatars/'
 MEDIA_ROOT = 'C:/Users/Stefan/Desktop/python/chat_app_backend/'

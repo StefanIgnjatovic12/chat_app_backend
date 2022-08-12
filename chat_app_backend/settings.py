eee90ea19306d96ea60b7299af877cbe9b566d8e
Stefan <sper1234@gmail.com>
2022-08-12 19:20:12 +0200

    Add whitenoise

--- chat_app_backend/settings.py
+++ chat_app_backend/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'dj_rest_auth.registration',
 
-
 ]
 
 SITE_ID = 1
@@ -55,13 +54,13 @@ SITE_ID = 1
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 
 ]
 

a5cfed2c995ab8d1f672ee8c2782fa7bf7d04678
Stefan <sper1234@gmail.com>
2022-08-20 20:51:18 +0200

    Refactor

--- api/views.py
+++ api/views.py
@@ -218,14 +218,14 @@ def create_new_message(request):
 def get_user_profile(request, pk):
     profile = Profile.objects.get(user_id=pk)
     user = User.objects.get(id=pk)
-    s3 = boto3.resource('s3', aws_access_key_id=AWS_ACCESS_KEY_ID,
-                            aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    # s3 = boto3.resource('s3', aws_access_key_id=AWS_ACCESS_KEY_ID,
+    #                         aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
     session = boto3.Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
                             aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-    bucket = s3.Bucket(AWS_STORAGE_BUCKET_NAME)
-    for object in bucket.objects.all():
-        print('Bucket name: ' + object.bucket_name)
-        print('Object key: ' + object.key)
+    # bucket = s3.Bucket(AWS_STORAGE_BUCKET_NAME)
+    # for object in bucket.objects.all():
+    #     print('Bucket name: ' + object.bucket_name)
+    #     print('Object key: ' + object.key)
     # Using the bucket name above
     if profile.real_avatar.url:
         print(profile.real_avatar.url)
@@ -248,7 +248,7 @@ def get_user_profile(request, pk):
         #                           session.client(
         #                               's3')}) \
         #         as image_file_2:
-        with smart_opener('s3://bucketeer-0f6cb5f5-34a1-49a1-ab57-f884d7245601/media/public/avatars/stefan.png', "rb",
+        with smart_opener('s3://bucketeer-0f6cb5f5-34a1-49a1-ab57-f884d7245601/bucketeer-0f6cb5f5-34a1-49a1-ab57-f884d7245601/media/public/avatars/stefan.png', "rb",
                           transport_params={
                               'client':
                                   session.client(

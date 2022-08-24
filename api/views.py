f86b1e0504cdb44eb06b7cf1c301594ff819d653
Stefan <sper1234@gmail.com>
2022-08-24 17:00:47 +0200

    Refactor

--- api/views.py
+++ api/views.py
@@ -14,6 +14,7 @@ import random
 import requests
 from smart_open import open as smart_opener
 from decouple import config
+from utils.try_except_block import AvatarsTryExceptBlock
 
 AWS_ACCESS_KEY_ID = config('BUCKETEER_AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('BUCKETEER_AWS_SECRET_ACCESS_KEY')
@@ -220,34 +221,34 @@ def get_user_profile(request, pk):
     user = User.objects.get(id=pk)
     session = boto3.Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
                             aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-
-    try:
-        with smart_opener(f's3://bucketeer-0f6cb5f5-34a1-49a1-ab57-f884d7245601/bucketeer-0f6cb5f5-34a1-49a1-ab57'
-                          f'-f884d7245601/media/public/avatars/{user.username}_default_avatar{profile.extension()}',
-                          "rb",
-                          transport_params={
-                              'client':
-                                  session.client(
-                                      's3')}) \
-                as image_file_2:
-            encoded_default_avatar = base64.b64encode(image_file_2.read())
-    except:
-        with open('avatars/default_avatar.png', "rb") as image_file:
-            encoded_default_avatar = base64.b64encode(image_file.read())
-
-    try:
-        with smart_opener(f's3://bucketeer-0f6cb5f5-34a1-49a1-ab57-f884d7245601/bucketeer-0f6cb5f5-34a1-49a1-ab57'
-                          f'-f884d7245601/media/public/real_avatars/{user.username}_real_avatar{profile.extension()}',
-                          "rb",
-                          transport_params={
-                              'client':
-                                  session.client(
-                                      's3')}) \
-                as image_file_2:
-            encoded_real_avatar = base64.b64encode(image_file_2.read())
-    except:
-        with open('avatars/default_avatar.png', "rb") as image_file:
-            encoded_real_avatar = base64.b64encode(image_file.read())
+    encoded_default_avatar, encoded_real_avatar = AvatarsTryExceptBlock(user)
+    # try:
+    #     with smart_opener(f's3://bucketeer-0f6cb5f5-34a1-49a1-ab57-f884d7245601/bucketeer-0f6cb5f5-34a1-49a1-ab57'
+    #                       f'-f884d7245601/media/public/avatars/{user.username}_default_avatar{profile.extension()}',
+    #                       "rb",
+    #                       transport_params={
+    #                           'client':
+    #                               session.client(
+    #                                   's3')}) \
+    #             as image_file_2:
+    #         encoded_default_avatar = base64.b64encode(image_file_2.read())
+    # except:
+    #     with open('avatars/default_avatar.png', "rb") as image_file:
+    #         encoded_default_avatar = base64.b64encode(image_file.read())
+    #
+    # try:
+    #     with smart_opener(f's3://bucketeer-0f6cb5f5-34a1-49a1-ab57-f884d7245601/bucketeer-0f6cb5f5-34a1-49a1-ab57'
+    #                       f'-f884d7245601/media/public/real_avatars/{user.username}_real_avatar{profile.extension()}',
+    #                       "rb",
+    #                       transport_params={
+    #                           'client':
+    #                               session.client(
+    #                                   's3')}) \
+    #             as image_file_2:
+    #         encoded_real_avatar = base64.b64encode(image_file_2.read())
+    # except:
+    #     with open('avatars/default_avatar.png', "rb") as image_file:
+    #         encoded_real_avatar = base64.b64encode(image_file.read())
 
     return Response([{
         'age': profile.age,
@@ -545,14 +546,11 @@ def edit_profile(request):
 
     # for when user is editing their existing profile
     if Profile.objects.filter(user=user).exists():
-        # if user.profile.exists():
-        # print('profile exists')
         profile = user.profile
+
     # for when user has just signed up and is adding profile details for the first time
     else:
-        # print('profile doesnt exist, creating profile')
         profile = Profile.objects.create(user=user)
-        # print(profile)
 
     serializer = ProfileSerializer(instance=profile, data=filtered_data, partial=True)
     if serializer.is_valid(raise_exception=True):
---
+++ utils/try_except_block.py
@@ -0,0 +1,41 @@
+import boto3
+from decouple import config
+from smart_open import open as smart_opener
+import base64
+AWS_ACCESS_KEY_ID = config('BUCKETEER_AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('BUCKETEER_AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('BUCKETEER_BUCKET_NAME')
+AWS_S3_REGION_NAME = config('BUCKETEER_AWS_REGION')
+session = boto3.Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
+                            aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+def AvatarsTryExceptBlock(user):
+    try:
+        with smart_opener(f's3://bucketeer-0f6cb5f5-34a1-49a1-ab57-f884d7245601/bucketeer-0f6cb5f5-34a1-49a1-ab57'
+                          f'-f884d7245601/media/public/avatars/{user.username}_default_avatar{profile.extension()}',
+                          "rb",
+                          transport_params={
+                              'client':
+                                  session.client(
+                                      's3')}) \
+                as image_file_2:
+            encoded_default_avatar = base64.b64encode(image_file_2.read())
+    except:
+        with open('avatars/default_avatar.png', "rb") as image_file:
+            encoded_default_avatar = base64.b64encode(image_file.read())
+
+    try:
+        with smart_opener(f's3://bucketeer-0f6cb5f5-34a1-49a1-ab57-f884d7245601/bucketeer-0f6cb5f5-34a1-49a1-ab57'
+                          f'-f884d7245601/media/public/real_avatars/{user.username}_real_avatar{profile.extension()}',
+                          "rb",
+                          transport_params={
+                              'client':
+                                  session.client(
+                                      's3')}) \
+                as image_file_2:
+            encoded_real_avatar = base64.b64encode(image_file_2.read())
+    except:
+        with open('avatars/default_avatar.png', "rb") as image_file:
+            encoded_real_avatar = base64.b64encode(image_file.read())
+
+    return encoded_default_avatar, encoded_real_avatar
